07493c67463d5aab905291893287ff446ce92840
hogehogei <matsuryo00@gmail.com>
2023-07-12 21:52:39 +0900

    Initial commit.

---
+++ CH32V003J4M6/gaming_led/blink.c
@@ -0,0 +1,33 @@
+#include "ch32v003fun.h"
+#include <stdint.h>
+
+int main()
+{
+	SystemInit();
+
+	// Enable GPIOC
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD;
+
+	// GPIO C1 Push-Pull
+	GPIOC->CFGLR &= ~(0xF<<(4*1));
+	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*1);
+
+	// GPIO C2 Push-Pull
+	GPIOC->CFGLR &= ~(0xf<<(4*2));
+	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*2);
+
+	// GPIO D6 Push-Pull
+	GPIOD->CFGLR &= ~(0xf<<(4*6));
+	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6);
+
+
+	while(1)
+	{
+		GPIOC->BSHR = 0x00000006;		// Set GPIO C1, C2
+		GPIOD->BSHR = 0x00000040;		// Set GPIO D6
+		Delay_Ms( 250 );
+		GPIOC->BSHR = 0x00600000;		// Clear GPIO C1, C2
+		GPIOD->BSHR = 0x04000000;		// Clear GPIO D6
+		Delay_Ms( 250 );
+	}
+}
